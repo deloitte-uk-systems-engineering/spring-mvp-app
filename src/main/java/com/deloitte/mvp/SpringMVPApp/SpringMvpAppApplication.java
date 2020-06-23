ace629190265ad33e105617c89b419192c539dc7
Dishansh <dish.gaurav@gmail.com>
2020-06-23 12:22:18 +0100

    initial project setup added

---
+++ src/main/java/com/deloitte/mvp/SpringMVPApp/SpringMvpAppApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.mvp.SpringMVPApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvpAppApplication.class, args);
+	}
+
+}
